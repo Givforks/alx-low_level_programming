d89b1a86fc4abd539fe3ab6c1fc6a568c7623f23
givforks <givens.abraham@live.com>
2023-09-07 08:42:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program print a line with puts function
+ * Return: 0 (completed)
+ */
+
+int main(voi)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

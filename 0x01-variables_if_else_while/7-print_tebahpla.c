28ace439912734ce284a94061a41ce798b9b5dc1
givforks <givens.abraham@live.com>
2023-09-08 21:24:03 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - modulus program function for entry point value
+ * Return: 0 void return type (completed)
+ */
+int main(void)
+{
+	char B;
+
+	B = 'z';
+	while
+		(B >= 'a')
+		{
+			putchar(B);
+			B--;
+		}
+	/* decrement reserve print lower case */
+	putchar('\n');
+	return (0);
+}

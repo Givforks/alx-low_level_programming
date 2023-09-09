313cf99a050c05e95be02c437621bdf1dd5b7db6
givforks <givens.abraham@live.com>
2023-09-08 20:57:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A main pgrogram with entry point function
+ * Return: 0 value return (completed)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	       printf("%d", k);
+	/* making use of format specifier */
+	putchar('\n');
+return (0);
+}

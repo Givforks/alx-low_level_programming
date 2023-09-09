c346d3d31cc2956b8aae744c802436c1b56595d8
givforks <givens.abraham@live.com>
2023-09-08 21:08:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the main function include an empty entry
+ * Return: 0 null (completed)
+ */
+int main(void)
+{
+	int r;
+
+	r = 0;
+	while
+		(r < 10)
+		{
+			putchar(r + '0');
+			r++;
+		}
+		/* r takes the increment until the codition satisfy */
+	putchar('\n');
+	return (0);
+}

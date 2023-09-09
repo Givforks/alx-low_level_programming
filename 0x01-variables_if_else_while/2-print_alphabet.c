ecc2ea016e702885d7faf48b367a326524e61405
givforks <givens.abraham@live.com>
2023-09-08 19:01:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point body function program void
+ * Return: Everytime return 0 (Completed)
+ */
+int main(void)
+{
+		char t;
+		/*  var_t,'' quote will locate ascii char decimal value */
+		t = 'a';
+		while
+			(t <= 'z')
+				{
+					putchar(t);
+					t++;
+				}
+
+		putchar('\n');
+		return (0);
+}

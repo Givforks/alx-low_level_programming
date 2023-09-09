48e16e94bbb5031b727033597b5d42a8445f1e30
givforks <givens.abraham@live.com>
2023-09-08 20:23:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A main function entry point program break and continue loop
+ * Return: 0 the return (completed)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+		/* declear variable and use while loop with two putchar */
+		while
+			(l <= 'z')
+				{
+			if ((l != 'q' && l != 'e') && l <= 'z')
+			putchar(l);
+			l++;
+				}
+		putchar('\n');
+			return (0);
+}
+
+

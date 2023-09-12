0b259a95dae7261881dcd5aa6ce749190b8eaef5
givforks <givens.abraham@live.com>
2023-09-12 16:51:15 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program function entry point main
+ * Return: 0 null (completed)
+ */
+int main(void)
+{
+/* _putchar print single letter */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}

01cf2ec5b788b7d0169227a8e4c291041048e0e1
givforks <givens.abraham@live.com>
2023-09-25 13:36:27 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to output
+ * @c: the letter to print
+ * Return: on completed 1
+ * On error -1 is returned and error number is set appprox
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));

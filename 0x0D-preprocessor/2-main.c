d75664011c14534ab37d04dfcd50d57615188ecd
givforks <givens.abraham@live.com>
2023-10-10 18:55:26 -0700

    printf

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - file print name
+ * Return: null 0 completely
+ *
+ */
+int main(void)
+{
+	printf("%s\n", FILE);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTON_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
